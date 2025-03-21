0664715dee4bd444487aa0b08b2f58bdb571c45c
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-03-21 12:26:44 +0530

    page(input-output): completed console input and output section of the page, redesigned the code scrollbar

--- src/pages/Sidebar/InputAndOutput/CodeSnippet.js
+++ src/pages/Sidebar/InputAndOutput/CodeSnippet.js
@@ -1,3 +1,6 @@
 export const INPUT_AND_OUTPUT = {
   INPUT_PROMPT: `const user = prompt('Please enter your username', 'user');`,
+  OUTPUT_LOG: `console.log("This is a regular log message.");`,
+  OUTPUT_WARN: `console.warn("This is a warning.");`,
+  OUTPUT_ERROR: `console.error("This is an error message.");`,
 };
--- src/pages/Sidebar/InputAndOutput/InputAndOutput.jsx
+++ src/pages/Sidebar/InputAndOutput/InputAndOutput.jsx
@@ -8,7 +8,7 @@ import useThemeStore from '../../../utils/Store/themeStore';
 import Footer from '../../../components/Footer/Footer';
 import CodeHighlighter from '../../../utils/CodeHighlighter/CodeHighlighter';
 
-// CODE SNIPPET 
+// CODE SNIPPET
 import { INPUT_AND_OUTPUT } from './CodeSnippet';
 
 const CONTAINER_VARIANTS = {
@@ -33,6 +33,7 @@ const getThemeStyles = (theme) => ({
   headlineFour:
     theme === 'light' ? 'headline-four headline-four-light' : 'headline-four headline-four-dark',
   paragraph: theme === 'light' ? 'paragraph-document-light' : 'paragraph-document-dark',
+  token: theme === 'light' ? 'token-light' : 'token-dark',
 });
 
 const InputAndOutput = () => {
@@ -65,20 +66,75 @@ const InputAndOutput = () => {
           applications. This chapter focuses on the various methods available in JavaScript for
           receiving input from users and displaying output.
         </p>
-        <h2 className={`${themeStyles.headlineTwo} border-l-4 border-green-600 pl-4 mb-3`}>
+        <h2 className={`${themeStyles.headlineTwo} border-l-3 border-green-600 pl-1.5 mb-3`}>
           CONSOLE INPUT AND OUTPUT
         </h2>
-        <h3 className={`${themeStyles.headlineFour} border-l-4 border-transparent pl-4`}>
-          CONSOLE OUTPUT
+        <h3 className={`${themeStyles.headlineFour} border-l-3 border-transparent pl-1.5`}>
+          CONSOLE INPUT
         </h3>
-        <p className={`${themeStyles.paragraph} border-l-4 border-transparent pl-4`}>
+        <p className={`${themeStyles.paragraph} border-l-3 border-transparent pl-1.5`}>
           Unlike some programming environments, JavaScript in browsers doesn't support direct
           console input. However, you can simulate input using built-in dialog functions such as:
         </p>
-        <div className="mb-6 pl-4 border-l-4 border-transparent">
+        <div className="mb-6 pl-1.5 border-l-3 border-transparent">
           <div className="overflow-x-auto">
             <div className="min-w-full">
-              <CodeHighlighter code={INPUT_AND_OUTPUT.INPUT_PROMPT} language="javascript" title="prompt.js" />
+              <CodeHighlighter
+                code={INPUT_AND_OUTPUT.INPUT_PROMPT}
+                language="javascript"
+                title="prompt.js"
+              />
+            </div>
+          </div>
+        </div>
+        <h3 className={`${themeStyles.headlineFour} pl-1.5 border-l-4 border-transparent`}>
+          CONSOLE OUTPUT
+        </h3>
+        <div className="border-l-3 border-transparent pl-1.5">
+          <p className={`${themeStyles.paragraph} `}>
+            The simplest form of output in javascript is to use the browser console. Developers use
+            it for debugging purposes and even sometimes display status messages. Commonly methods
+            include the following :
+          </p>
+          <ul>
+            <li className={`${themeStyles.paragraph}`}>
+              <span className={`${themeStyles.token}`}> console.log() </span> &nbsp; : Prints
+              particular messages to the browser console.
+            </li>
+            <li className={`${themeStyles.paragraph}`}>
+              <span className={`${themeStyles.token}`}> console.warn() </span> &nbsp; : Prints
+              warning messages.
+            </li>
+            <li className={`${themeStyles.paragraph}`}>
+              <span className={`${themeStyles.token}`}> console.error() </span> &nbsp; : Prints
+              error messages.
+            </li>
+          </ul>
+          <div className="overflow-x-auto mb-3">
+            <div className="min-w-full">
+              <CodeHighlighter
+                code={INPUT_AND_OUTPUT.OUTPUT_LOG}
+                language="javascript"
+                title="log.js"
+              />
+            </div>
+          </div>
+          <div className="overflow-x-auto mb-3">
+            <div className="min-w-full">
+              <CodeHighlighter
+                code={INPUT_AND_OUTPUT.OUTPUT_WARN}
+                language="javascript"
+                title="warning.js"
+              />
+            </div>
+          </div>
+          <div className="overflow-x-auto mb-3">
+            <div className="min-w-full">
+              <CodeHighlighter
+                code={INPUT_AND_OUTPUT.OUTPUT_ERROR}
+                language="javascript"
+                title="error.js"
+              />
             </div>
           </div>
         </div>
